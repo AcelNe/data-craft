7a278daed59d016fb2c141789e3e76ad06cd6da1
MONINGSUN <moningsun1021@gmail.com>
2024-07-23 10:56:46 +0900

    JSch 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,14 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    //json
     implementation("com.google.code.gson:gson:2.11.0")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.14.1")
     implementation("com.googlecode.json-simple:json-simple:1.1.1")
+
+    //jsch
+    implementation("com.jcraft:jsch:0.1.55")
 
 }
 
