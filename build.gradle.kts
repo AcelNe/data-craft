a97054820c69a04d8f24e7dcdbcdfe95849352ec
MONINGSUN <moningsun1021@gmail.com>
2024-12-03 15:43:09 +0900

    lombok 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,10 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // Lombok 의존성 추가
+    compileOnly("org.projectlombok:lombok:1.18.24")  // Lombok을 컴파일 타임에만 사용
+    annotationProcessor("org.projectlombok:lombok:1.18.24")  // Lombok 어노테이션 처리기
 }
 
 tasks.test {
