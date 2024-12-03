a7049ad9afe0d52c6a26cbac3cb94c56b0edba0b
MONINGSUN <moningsun1021@gmail.com>
2024-12-03 15:43:17 +0900

    dto 내용 추가

--- src/main/java/dto/User.java
+++ src/main/java/dto/User.java
@@ -1,4 +1,12 @@
 package dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class User {
+    private String name;
+    private String age;
+    private String addr;
 }
